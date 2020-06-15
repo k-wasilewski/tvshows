97058f83f319efa29ee0c52834fe89984d9d6b11
k-wasilewski <k.k.wasilewski@gmail.com>
2020-06-15 18:03:56 +0200

    results filtering

--- src/components/Results.js
+++ src/components/Results.js
@@ -3,12 +3,36 @@ import '../css/App.css';
 import {setResults} from "../redux/actions";
 import {connect} from "react-redux";
 import ResultsTable from "./ResultsTable";
+import ResultsFilter from "./ResultsFilter";
+import mapDayNoToName from "../functions/mapDayNoToName";
 
 class Results extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            filteredResults: []
+        };
+    }
+
+    filterByDay = (day) => {
+        const dayName = mapDayNoToName(day);
+
+        const filteredResults = this.props.results.filter(result =>
+            result.show.schedule.days.includes(dayName)
+        );
+
+        this.setState({filteredResults: filteredResults});
+    }
+
     render() {
         return (
             <div className="App">
-                <ResultsTable results={this.props.results} />
+                <ResultsFilter setDay={this.filterByDay} />
+                <ResultsTable results={(this.state.filteredResults.length===0) ?
+                    this.props.results
+                    :
+                    this.state.filteredResults} />
             </div>
         );
     };
---
+++ src/components/ResultsFilter.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+
+const useStyles = makeStyles((theme) => ({
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 120,
+    },
+    selectEmpty: {
+        marginTop: theme.spacing(2),
+    },
+}));
+
+export default function SimpleSelect(props) {
+    const classes = useStyles();
+    const [day, setDay] = React.useState('');
+
+    const handleChange = (event) => {
+        setDay(event.target.value);
+        props.setDay(event.target.value);
+    };
+
+    return (
+        <div>
+            <FormControl className={classes.formControl}>
+                <InputLabel id="demo-simple-select-label">Dzień tygodnia</InputLabel>
+                <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={day}
+                    onChange={handleChange}
+                >
+                    <MenuItem value={1}>Poniedziałek</MenuItem>
+                    <MenuItem value={2}>Wtorek</MenuItem>
+                    <MenuItem value={3}>Środa</MenuItem>
+                    <MenuItem value={4}>Czwartek</MenuItem>
+                    <MenuItem value={5}>Piątek</MenuItem>
+                    <MenuItem value={6}>Sobota</MenuItem>
+                    <MenuItem value={7}>Niedziela</MenuItem>
+                </Select>
+            </FormControl>
+        </div>
+    );
+}
