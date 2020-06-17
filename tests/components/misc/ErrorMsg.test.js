eb834eb0127f1a9946aa1359ef7c789f83242a61
k-wasilewski <k.k.wasilewski@gmail.com>
2020-06-17 15:11:50 +0200

    ErrorMsg.test added

---
+++ tests/components/misc/ErrorMsg.test.js
@@ -0,0 +1,29 @@
+import renderer from "react-test-renderer";
+import {ErrorMsg} from "../../../src/components/misc/ErrorMsg";
+import React from "react";
+import {configure, mount} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+describe("ErrorMsg rendering specification", () => {
+    it('ErrorMsg is rendered', () => {
+        const component = renderer.create(
+            <ErrorMsg/>
+        );
+        const tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+    describe("ErrorMsg functional specification", () => {
+        it('renders msg passed as props', () => {
+            configure({adapter: new Adapter()});
+
+            const testMsg = 'test message';
+
+            const component = mount(
+                <ErrorMsg msg={testMsg} />
+            );
+
+            expect(component.find('span').text()).toBe(testMsg);
+        });
+    });
+});
