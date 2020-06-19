45a7fe8a0d5a3e9f7d728062684d6288357627bd
k-wasilewski <k.k.wasilewski@gmail.com>
2020-06-19 14:54:52 +0200

    Details.test test added

--- tests/components/details/Details.test.js
+++ tests/components/details/Details.test.js
@@ -7,13 +7,25 @@ import {Provider} from "react-redux";
 import {configure, mount} from 'enzyme';
 import Adapter from "enzyme-adapter-react-16";
 import {ErrorMsg} from "../../../src/components/misc/ErrorMsg";
+import Button from '@material-ui/core/Button';
 
 describe("Details rendering specification", () => {
     it('Details is rendered', () => {
+        const testResult = {
+            show: {
+                name: 'sample name',
+                image: {
+                    medium: 'mediumSrc',
+                    original: 'originalSrc'
+                }
+            },
+            score: 2
+        };
+
         const component = renderer.create(
             <Provider store={store}>
                 <BrowserRouter>
-                    <ConnectedDetails />
+                    <ConnectedDetails detailedResult={testResult} />
                 </BrowserRouter>
             </Provider>
         );
@@ -21,6 +33,15 @@ describe("Details rendering specification", () => {
         expect(tree).toMatchSnapshot();
     });
 });
+
+const mockHistoryPush = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useHistory: () => ({
+        push: mockHistoryPush,
+    }),
+}));
 
 describe("Details functional specification", () => {
     it('renders ErrorMsg when props.detailedResult value is empty', (done) => {
@@ -55,6 +76,28 @@ describe("Details functional specification", () => {
 
         setTimeout(function () {
             expect(component.find(ErrorMsg).debug()).toBe('');
+            component.unmount();
+            done();
+        }, 500);
+    });
+
+    it('redirects to \' when goBackBtn is clicked', (done) => {
+        configure({adapter: new Adapter()});
+
+        const component = mount(
+            <Provider store={store}>
+                <BrowserRouter>
+                    <ConnectedDetails />
+                </BrowserRouter>
+            </Provider>
+        );
+
+        const goBackBtn = component.find(Button).at(0);
+        goBackBtn.simulate('click');
+        component.update();
+
+        setTimeout(function () {
+            expect(mockHistoryPush).toHaveBeenCalledWith('/');
             component.unmount();
             done();
         }, 500);
