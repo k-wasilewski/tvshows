2ce25671921129ef91b4787d0ae46bc22a578b76
k-wasilewski <k.k.wasilewski@gmail.com>
2020-06-20 17:37:19 +0200

    suppressing console.error logs in test

--- tests/components/error/ErrorBoundary.test.js
+++ tests/components/error/ErrorBoundary.test.js
@@ -4,22 +4,25 @@ import Adapter from "enzyme-adapter-react-16";
 import ErrorBoundary from "../../../src/components/error/ErrorBoundary";
 
 describe("ErrorBoundary functional specification", () => {
-    function ErrorComponent() {
+    function TestErrorComponent() {
         throw new Error('Error thrown from ErrorComponent');
         return (<div>Error</div>);
     }
 
-    it('ErrorComponent is rendered when error appears', () => {
+    it('catches error when it appears', () => {
         configure({adapter: new Adapter()});
 
         const componentDidCatch = jest.spyOn(ErrorBoundary.prototype, 'componentDidCatch');
+        const consoleErrorLog = jest.spyOn(console, 'error')
+            .mockImplementation(() => console.log('error caught by ErrorBoundary'));
 
         const component = mount(
             <ErrorBoundary>
-                <ErrorComponent />
+                <TestErrorComponent />
             </ErrorBoundary>
         );
 
         expect(componentDidCatch).toHaveBeenCalled();
+        expect(consoleErrorLog).toHaveBeenCalled();
     });
 });
