1126b4f018af77fa2d84d1590741a2fee44dd877
k-wasilewski <k.k.wasilewski@gmail.com>
2020-06-20 11:40:41 +0200

    corrected typo in imports

--- src/components/error/NotFoundComponent.js
+++ src/components/error/NotFoundComponent.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {styles} from "../../styles/styles";
 import {useHistory} from 'react-router-dom';
-import {ErrorMsg} from './/ErrorMsg';
+import {ErrorMsg} from './ErrorMsg';
 import Button from '@material-ui/core/Button';
 
 export function NotFoundComponent(props) {
