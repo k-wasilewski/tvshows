aab219d2dee130b5b54d6cb2b7b2982673a816ee
k-wasilewski <k.k.wasilewski@gmail.com>
2020-06-21 19:36:59 +0200

    separate build and build_gzip directories for surge and node.js deployment

--- server.js
+++ server.js
@@ -3,14 +3,6 @@ const path = require('path');
 const app = express();
 const expressStaticGzip = require('express-static-gzip');
 
-app.use(
-    expressStaticGzip(path.join(__dirname, 'build')),
-);
-
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+app.use("/", expressStaticGzip(path.join(__dirname, 'build_gzip')));
 
 app.listen(3000);
