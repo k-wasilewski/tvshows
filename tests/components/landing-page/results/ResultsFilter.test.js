e28702aa7c7aaa1cd3b06c9c4164dd5d2edf4933
k-wasilewski <k.k.wasilewski@gmail.com>
2020-07-17 17:10:31 +0200

    extended ResultsFilter.test

--- tests/components/landing-page/results/ResultsFilter.test.js
+++ tests/components/landing-page/results/ResultsFilter.test.js
@@ -6,6 +6,7 @@ import {configure, mount} from 'enzyme';
 import Adapter from "enzyme-adapter-react-16";
 import store from "../../../../src/redux/store";
 import {Provider} from "react-redux";
+import {Button} from "@material-ui/core";
 
 describe("ResultsFilter rendering specification", () => {
     it('ResultsFilter is rendered', () => {
@@ -52,7 +53,7 @@ describe("ResultsFilter functional specification", () => {
         }, 500);
     });
 
-    it('sets state.day value and calls props.setDay when day input is changed', () => {
+    it('sets state.day value and calls props.setDay when day input is changed', (done) => {
         const mockSetDayState = jest.spyOn(React, 'useState');
         const mockSetDayProps = jest.fn();
 
@@ -63,12 +64,37 @@ describe("ResultsFilter functional specification", () => {
         expect(mockSetDayProps).toHaveBeenCalledTimes(0);
         expect(mockSetDayState).toHaveBeenCalledTimes(1);
 
-        component.find('#resultsFilterSelect').at(0).simulate('change', {target: { value: 3}});
+        component.find('input').at(0).simulate('change', {target: { value: 3}});
         component.update();
 
         setTimeout(function () {
             expect(mockSetDayProps).toHaveBeenCalledTimes(1);
             expect(mockSetDayState).toHaveBeenCalledTimes(2);
+
+            done();
+        }, 500);
+    });
+
+    it('button calls props doReset() and resets state value day when clicked', (done) => {
+        const mockDoReset = jest.fn();
+
+        component = mount(
+            <ResultsFilter doReset={mockDoReset} />
+        );
+
+        component.find('#resultsFilterSelect').at(0).props().value=3;
+
+        expect(mockDoReset).toHaveBeenCalledTimes(0);
+        expect(component.find('#resultsFilterSelect').at(0).props().value).toEqual(3);
+
+        component.find(Button).at(0).simulate('click');
+        component.update();
+
+        setTimeout(function () {
+            expect(mockDoReset).toHaveBeenCalledTimes(1);
+            expect(component.find('#resultsFilterSelect').at(0).props().value).toEqual('');
+
+            done();
         }, 500);
     });
 });
