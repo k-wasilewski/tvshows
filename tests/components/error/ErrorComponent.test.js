4e582615c69f4734cac202c6337005a4f15c194e
k-wasilewski <k.k.wasilewski@gmail.com>
2020-07-17 16:15:51 +0200

    extended ErrorComponent.test

--- tests/components/error/ErrorComponent.test.js
+++ tests/components/error/ErrorComponent.test.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import {ErrorComponent} from "../../../src/components/error/ErrorComponent";
+import {configure, mount} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import {Button} from "@material-ui/core";
+import {Router} from "react-router-dom";
 
 describe("ErrorComponent rendering specification", () => {
     it('ErrorComponent is rendered', () => {
@@ -9,5 +13,29 @@ describe("ErrorComponent rendering specification", () => {
         );
         const tree = component.toJSON();
         expect(tree).toMatchSnapshot();
+    });
+});
+
+describe("ErrorComponent functional specification", () => {
+    it('button onClick redirects to landing page onClick', (done) => {
+        configure({adapter: new Adapter()});
+
+        const historyMock = { push: jest.fn(), location: {}, listen: jest.fn() };
+
+        const component = mount(
+            <Router history={historyMock}>
+                <ErrorComponent msg='Nie znaleziono strony' />
+            </Router>
+        );
+
+        const returnBtn = component.find(Button).at(0);
+        returnBtn.simulate('click');
+        component.update();
+
+        setTimeout(function () {
+            expect(historyMock.push).toHaveBeenCalledWith('/');
+
+            done();
+        }, 500);
     });
 });
