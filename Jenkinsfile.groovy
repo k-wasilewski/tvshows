4fd1103476a5411367a86cfd59a0a9673da5a6ed
Wasilewski <jakub.wasilewski.ctr@sabre.com>
2022-03-25 12:20:42 +0100

    Jenkinsfile fix

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -5,36 +5,27 @@ import ci.utils.plab.v2.model.loaddrivers.*
 import ci.utils.plab.v2.model.reportData.*
 import ci.utils.plab.v2.model.test.*
 
-import java.time.OffsetDateTime
-import java.time.ZoneId
-import java.time.format.DateTimeFormatter
-
-//Currently, AWS Nexus repository is used to download the maven dependencies as they are not available in GCP Nexus repository
-SABRE_NEXUS_URL = "repository.sabre.com"
-NEXUS_URL = "repository.sabre-gcp.com"
-ARMADA_URL = "https://armada-backend-ngp-armada.apps.ocusw2.scs.dev.ascint.sabrecirrus.com"
-SNOW_ASSET_TAG = "SBI:TAB-GCP"
-GKE_PROJECT = "sab-dev-cluster-2713"
-CLUSTER_NAME = "dev-01-a9wx"
-TEMP_DEPLOY_STRATEGY = "com.sabre.ngp-cicd:blue-green:4.0.12"
-
 pipeline {
     agent {
-        kubernetes {
-            yamlFile './pod.yaml'
-            idleMinutes 60
-            instanceCap 3
-            workingDir '/tmp'
-            label 'tvshows-agent'
-        }
+        docker { image 'gcr.io/cloud-builders/yarn' }
     }
 
     stages {
+        stage('git clone repository') {
+            steps {
+                script {
+                    dir("/tmp") {
+                        sh(returnStdout: true, script: 'git clone https://github.com/k-wasilewski/tvshows.git')
+                    }
+                }
+            }
+        }
+
         stage('app install') {
             steps {
                 container('yarn') {
                     script {
-                        dir("/tmp") {
+                        dir("/tmp/tvshows") {
                             sh(returnStdout: true, script: 'ls')
                             sh(returnStdout: true, script: 'yarn install')
                         }
@@ -47,7 +38,7 @@ pipeline {
             steps {
                 container('yarn') {
                     script {
-                        dir("/tmp") {
+                        dir("/tmp/tvshows") {
                             sh(script: 'yarn start')
                         }
                     }
@@ -59,7 +50,7 @@ pipeline {
             steps {
                 container('yarn') {
                     script {
-                        dir("/tmp/e2e-tests") {
+                        dir("/tmp/tvshows/e2e-tests") {
                             sh(returnStdout: true, script: 'ls')
                             sh(returnStdout: true, script: 'yarn install')
                         }
@@ -74,7 +65,7 @@ pipeline {
                     script {
                         def serviceHost = "http://localhost:3000"
                         def testSuites = "cypress/integration/main_spec.js"
-                        dir("/tmp/e2e-tests") {
+                        dir("/tmp/tvshows/e2e-tests") {
                             sh(script: 'yarn install')
                             sh(script: "yarn run cypress run --config baseUrl=${serviceHost} --spec ${testSuites}")
                         }
